e2cd346fc03a1cba121298ea31f0d9af5169a623
Prashant <you@example.com>
2022-07-11 12:30:16 +0545

    Todo App in React

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import ReactDOM from "react-dom";
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+import App from "./App.js"; 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <BrowserRouter> */}
+      <App />
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
+);  
